7156d32dac0cb74d22317824f1daacbda10fec6b
Feng Chiu <feng.chiu@nyu.edu>
2024-10-09 15:15:31 +0800

    Finish task 4 of project
    Implemented differentiated payment and made changes to pass project tests.
    
    - Added differentiated payment cli feature and function
    - Added cli checks for incorrect parameters
    - Changed --type arg to accept any string
    - Changed how values are printed so tests can pass

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -8,7 +8,7 @@ parser.add_argument('--payment', type=float, help='Monthly payment amount')
 parser.add_argument('--principal', type=float, help='Principal loan amount')
 parser.add_argument('--periods', type=int, help='Number of payment periods')
 parser.add_argument('--interest', type=float, help='Loan interest rate as a percentage')
-parser.add_argument('--type', choices=['annuity', 'diff'], help='Type of payment: "annuity" or "diff"')
+parser.add_argument('--type', type=str, help='Type of payment: "annuity" or "diff"')
 
 # Parse the arguments
 args = parser.parse_args()
@@ -20,7 +20,7 @@ def annuity(payment, principal, periods, interest):
     i = (interest * 0.01) / 12
 
     if not principal:
-        principal = p = payment / ((i * pow(1 + i, periods)) / (pow(1 + i, periods) - 1))
+        principal = p = payment / ((i * (1 + i)**periods) / ((1 + i)**periods - 1))
         print(f'Your loan principal = {principal}')
     elif not periods:
         n = math.log(payment / (payment - i * principal), 1 + i)
@@ -31,21 +31,31 @@ def annuity(payment, principal, periods, interest):
             print('It will take 1 month to repay the loan')
         else:
             years, months = divmod(periods, 12)
+            years, months = int(years), int(months)
             if months != 0 and years == 0:
                 print(f'It will take {months} months to repay the loan')
             elif months == 0 and years != 0:
                 print(f'It will take {years} years to repay the loan')
             elif months != 0 and years != 0:
                 print(f'It will take {years} years and {months} months to repay this loan')
+        # test#5 expects the overpayment AFTER time...
+        # and it hates floats...
+        print(f'Overpayment = {int(periods * payment - principal)}')
     elif not payment:
-        a = principal * (i * pow(1 + i, periods)) / (pow(1 + i, periods) - 1)
+        a = principal * (i * (1 + i)**periods) / ((1 + i)**periods - 1)
 
         fractional, payment = math.modf(a)
+
         if fractional != 0:
             payment, remain = payment + 1, (principal - (payment + 1) * (periods - 1))
-            print(f'Your monthly payment = {payment} and the last payment = {remain}.')
+            # print(f'Your monthly payment = {payment} and the last payment = {remain}.')
         else:
-            print(f'Your monthly payment = {payment}')
+            # print(f'Your monthly payment = {payment}')
+            pass
+        # test#3 changed how we are supposed to present the numbers...
+        print(f'Overpayment = {periods * payment - principal}')
+        print(f'Your monthly payment = {payment}')
+
 
 
 def differentiated(principal, periods, interest):
@@ -66,9 +76,13 @@ def valid_annuity_args():
     return len(none_args) == 1
 
 
-if mode == 'annuity' and valid_annuity_args() and interest:
+def args_are_positive():
+    return all(value > 0 for value in [payment, principal, periods, interest] if value is not None)
+
+
+if mode == 'annuity' and valid_annuity_args() and interest and args_are_positive():
     annuity(payment, principal, periods, interest)
-elif mode == 'diff' and not payment and principal and periods and interest:
+elif mode == 'diff' and not payment and principal and periods and interest and args_are_positive():
     differentiated(principal, periods, interest)
 else:
     print('Incorrect parameters')
